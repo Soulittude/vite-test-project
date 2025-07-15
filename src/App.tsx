2c8a645db3c2496d1408f6c7ca8de1a7a3f75425
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-07-15 21:43:59 +0300

    Change visibility of a component with dynamic buttons via statement usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
   let items = ['New York', "San Francisco", "Tokyo", "London", "Paris"];
   const handleSelectItem = (item: string) => {
     console.log(item);
   }
   return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
-    <Alert>
+    {alertVisible && <Alert onClose={() => setAlertVisibility(false)} alert-dismissible>
       Hello <span>World</span>
-    </Alert>
-    <Button color="secondary" onClick={() => console.log('Clicked')}>My Button</Button>
+    </Alert>}
+    <Button color="secondary" onClick={() => setAlertVisibility(true)}>My Button</Button>
   </div>
 }
 export default App;
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -2,11 +2,14 @@ import type { ReactNode } from "react";
 
 interface Props {
     children: ReactNode;
+    onClose: () => void;
 }
 
-const Alert = ({ children }: Props) => {
+const Alert = ({ children, onClose }: Props) => {
     return (
-        <div className="alert alert-primary">{children}</div>
+        <div className="alert alert-primary alert-dismissible">{children}
+            <button type="button" onClick={onClose} className="btn-close" data-bs-dismiss='alert' />
+        </div>
     )
 }
 
